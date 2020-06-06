430ed99d20299b4b1de8bca059a550b6420f1ab4
Bruno <bruno.cesar@cilia.com.br>
2020-06-06 11:38:12 -0300

    Arrow functions - map function

---
+++ ES6/arrow-functions/map/mapNewWay.js
@@ -0,0 +1,5 @@
+let sample = [1, 2, 3, 4, 5];
+
+let newDouble = sample.map(item => item * 2);
+
+console.log(newDouble);
---
+++ ES6/arrow-functions/map/mapOldWay.js
@@ -0,0 +1,7 @@
+let sample = [1, 2, 3, 4, 5];
+
+let double = sample.map(function (item) {
+    return item * 2;
+});
+
+console.log(double);
